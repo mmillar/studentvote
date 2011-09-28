e4598c843cfc14bc3c617761080cb6d91aea0477
Michael Millar <mmillar@verto.ca>
2011-09-28 12:44:37 -0400

    Made footer link clickable

--- code/wp-content/themes/studentvote/footer.php
+++ code/wp-content/themes/studentvote/footer.php
@@ -1,11 +1,11 @@
       <div id="footer">
         <?php wp_footer(); ?>
         <hr />
-        <div class="themark_footer" onclick="http://themarknews.com"></div>
+        <div class="themark_footer" onclick="window.location='http://www.themarknews.com';"></div>
         <?php wp_nav_menu(array('menu' => 'footer_menu')); ?>
         <script type="text/javascript">
           $(function() {
-          	$("#footer ul li:last-child").css("border-right","none");
+            $("#footer ul li:last-child").css("border-right","none");
           });
         </script>
       </div>
